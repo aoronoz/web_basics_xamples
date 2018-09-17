6ca24256de20e5dda2cbeee6e233b39215060a3e
Pedro Gomez <makhnnar@gmail.com>
2018-09-16 20:36:58 -0400

    added new test

--- prueba3/prueba4.js
+++ prueba3/prueba4.js
@@ -1,21 +1,62 @@
 
 var seleccion= function(){
 
-var entrada=document.getElementById('numero').value;
+	var entrada=document.getElementById('numero').value;
 
-
-
-var horas = entrada/3600;
+	var horas = entrada/3600;
+	
 	horas = Math.trunc(horas);
+	
 	console.log(horas);
-var residuoHoras = entrada%3600;
-var minutos = residuoHoras/60;
+	
+	var residuoHoras = entrada%3600;
+	
+	var minutos = residuoHoras/60;
+	
 	minutos = Math.trunc(minutos)
-var segundos= residuoHoras%60;
-console.log(minutos);
-console.log(segundos);
-console.log(horas+':'+minutos+':'+  segundos+'');
-document.getElementById('resultado').innerHTML= horas+':'+minutos+':'+  segundos+'';
+	
+	var segundos= residuoHoras%60;
+
+	console.log(minutos);
+	
+	console.log(segundos);
+	
+	console.log(horas+':'+minutos+':'+  segundos+'');
+
+	var horasMostrar='';
+	
+	if (horas===0) {
+	
+		horasMostrar= horas+'0';
+	
+	} else {
+	
+		horasMostrar= horas;
+	}
+
+	var minutosMostrar='';
+	
+	if (minutos===0) {
+	
+		minutosMostrar= minutos+'0';
+	
+	} else {
+	
+		minutosMostrar=minutos;
+	}
+
+	var segundosMostrar='';
+	
+	if (segundos===0) {
+	
+		segundosMostrar=segundos+'0';
+	} else {
+	
+		segundosMostrar=segundos;
+
+	}
+	
+	document.getElementById('resultado').innerHTML= horasMostrar+':'+minutosMostrar+':'+segundosMostrar;
 }
 
 
---
+++ prueba4/prueba4.js
@@ -0,0 +1,25 @@
+var entradaMinuscula = 'z';   
+
+
+if (entradaMinuscula==='a') {
+
+	console.log(entradaMinuscula+ '   es vocal');
+
+} else if (entradaMinuscula==='e'){
+
+	console.log(entradaMinuscula+ '    es vocal');
+
+} else if (entradaMinuscula==='i'){
+
+	console.log(entradaMinuscula+ '   es vocal');
+
+}else if (entradaMinuscula==='o'){
+
+	console.log(entradaMinuscula+ '   es vocal');
+
+} else if (entradaMinuscula==='u'){
+
+	console.log(entradaMinuscula+ '   es vocal');
+}
+
+
